0596a7dc48b684b47b4a263eb3c497a3dd6956d3
kei-kusanagi <kei_kusanagi@outlook.com>
2023-01-26 12:48:35 -0600

    fixed container, button and add new dicee images

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,18 @@
 // ignore_for_file: non_constant_identifier_names
-
+import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'dart:math';
+import 'package:rive/rive.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   return runApp(
     MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.purple,
+
         appBar: AppBar(
-          title: const Center(
-            child: Text('Dicee'),
-          ),
-          backgroundColor: Colors.green,
+          title: const Text('Dicee'),
+          backgroundColor: Colors.deepOrange,
         ),
         body: DicePage(),
       ),
@@ -28,6 +28,16 @@ class DicePage extends StatefulWidget {
 class _DicePageState extends State<DicePage> {
   int leftDiceNumber = 1;
   int rightDiceNumber = 1;
+  late RiveAnimationController _btnAnimationController;
+
+  @override
+  void initState() {
+    _btnAnimationController = OneShotAnimation(
+      "active",
+      autoplay: false,
+    );
+    super.initState();
+  }
 
   void randomNumber() {
     leftDiceNumber = Random().nextInt(6) + 1;
@@ -36,63 +46,89 @@ class _DicePageState extends State<DicePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Column(
-        children: <Widget>[
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextButton(
-                    onPressed: () {
-                      setState(
-                        () {
-                          leftDiceNumber = Random().nextInt(6) + 1;
-                        },
-                      );
-                    },
-                    child: Image.asset('images/dice$leftDiceNumber.png'),
-                  ),
-                ),
-                Expanded(
-                  child: TextButton(
-                    onPressed: () {
-                      setState(
-                        () {
-                          rightDiceNumber = Random().nextInt(6) + 1;
-                        },
-                      );
-                    },
-                    child: Image.asset('images/dice$rightDiceNumber.png'),
-                  ),
-                ),
-              ],
+    return Scaffold(
+      backgroundColor: Colors.purple.shade800,
+      body: Stack(
+        children: [
+          const RiveAnimation.asset("RiveAssets/reloadl.riv"),
+          Positioned.fill(
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+              child: const SizedBox(),
             ),
           ),
-          Expanded(
-            child: Row(
-              children: [
-                Expanded(
-                  child: TextButton(
-                    onPressed: () {
-                      setState(
-                        () {
-                          // rightDiceNumber = Random().nextInt(6) + 1;
+
+          Column(
+            children: [
+              const SizedBox(
+                height: 200,
+                width: 200,
+              ),
+              Center(
+                child: Row(
+                    children: [
+                      Expanded(
+                        child: TextButton(
+                          onPressed: () {
+                            setState(
+                              () {
+                                leftDiceNumber = Random().nextInt(6) + 1;
+                              },
+                            );
+                          },
+                          child: Image.asset('images/dice$leftDiceNumber.png'),
+                        ),
+                      ),
+                      Expanded(
+                        child: TextButton(
+                          onPressed: () {
+                            setState(
+                              () {
+                                rightDiceNumber = Random().nextInt(6) + 1;
+                              },
+                            );
+                          },
+                          child: Image.asset('images/dice$rightDiceNumber.png'),
+                        ),
+                      ),
+
+                    ],
+                ),
+              ),
+              Center(
+                  child: GestureDetector(
+                    onTap: (){
+                      _btnAnimationController.isActive = true;
+                      setState( () {
                           randomNumber();
                         },
                       );
                     },
-                    child: ListTile(
-                      leading: Icon(
-                        Icons.change_circle_outlined,
-                        color: Colors.cyan.shade400,
-                        size: 80.0,
+                    child: SizedBox(
+                      height: 200,
+                      width: 200,
+                      child: Stack(
+                        children: [
+                          RiveAnimation.asset("RiveAssets/button.riv",
+                          // RiveAnimation.asset("RiveAssets/reloadl.riv",
+                          controllers: [_btnAnimationController],
+                        ),
+                          Positioned.fill(child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              SizedBox(width: 8),
+                              Text("Tirar ambos",
+                              style: TextStyle(fontWeight: FontWeight.w800),
+                              ),
+                            ],
+                          ))
+                        ],
                       ),
+
                     ),
-                  ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ],
       ),
